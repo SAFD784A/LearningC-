d34b98ceb02a3b8e58dd876a93182ee83b52c272
Stephen Froidcoeur <sfroid3@gmail.com>
2020-06-22 15:45:28 -0500

    Init install

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,19 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard; 
+        }
+    }
+
+    
+}
